e789a295ab9fce327eb645cbaec365b2204d5674
Matthew Rosen <matthewrosen@Matthews-MacBook-Air.local>
2024-08-11 23:21:49 -0400

    delete original project

--- guru/apps/authentification/apps.py
+++
@@ -1,9 +0,0 @@
-# auth/apps.py
-
-from django.apps import AppConfig
-
-
-class AuthentificationConfig(AppConfig):
-    name = 'guru.apps.authentification'
-    verbose_name = 'authentification helper app'
-    label = 'authentification'
--- guru/apps/breakup/apps.py
+++
@@ -1,9 +0,0 @@
-# breakup/apps.py
-
-from django.apps import AppConfig
-
-
-class BreakupConfig(AppConfig):
-    name = 'guru.apps.breakup'
-    verbose_name = 'breakup helper app'
-    label = 'breakup'
